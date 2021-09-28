f8c43cb5e9bcfc466c0eeb4c5c893c95e9116451
Roger Marsh <roger.marsh@solentware.co.uk>
2021-09-28 16:47:13 +0000

    Adjust version to 1.2.2 in setup.py.
    Source distributions uploaded to solentware.co.uk.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     setup(
         name="chesscalc",
-        version="1.2.2.dev0",
+        version="1.2.2",
         description="Calculate player performances in chess games",
         author="Roger Marsh",
         author_email="roger.marsh@solentware.co.uk",
@@ -39,8 +39,8 @@ if __name__ == "__main__":
             "Intended Audience :: Developers",
             "Development Status :: 3 - Alpha",
         ],
-        install_requires=["solentware-misc==1.3.1.dev0"],
+        install_requires=["solentware-misc==1.3.1"],
         dependency_links=[
-            "http://solentware.co.uk/files/solentware-misc-1.3.1.dev0.tar.gz",
+            "http://solentware.co.uk/files/solentware-misc-1.3.1.tar.gz",
         ],
     )
